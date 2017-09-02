6cd75193b9ae33d3912d53359dad0d6d9e3f0620
Xing Zhou <xing@ohdyno.me>
2017-09-02 10:31:12 -0400

    Modify replaceWorld to use higher order function 'map' for looping through World.

--- src/main/kotlin/me/ohdyno/projects/life/simulate/ReplacesWorld.kt
+++ src/main/kotlin/me/ohdyno/projects/life/simulate/ReplacesWorld.kt
@@ -1,20 +1,10 @@
 package me.ohdyno.projects.life.simulate
 
 import me.ohdyno.projects.life.simulate.replace.ReplacesCell
-import me.ohdyno.projects.life.simulate.values.Coordinates
-import me.ohdyno.projects.life.simulate.replace.values.MutableWorld
 import me.ohdyno.projects.life.simulate.values.World
 
 class ReplacesWorld(private val replacesCell: ReplacesCell) {
     fun replace(oldWorld: World): World {
-        val newWorld = MutableWorld(oldWorld)
-        for (x in 0 until oldWorld.width) {
-            for (y in 0 until oldWorld.height) {
-                val coordinates = Coordinates(x, y)
-                val replacementCell = replacesCell.replace(oldWorld, coordinates)
-                newWorld.replace(coordinates, replacementCell)
-            }
-        }
-        return newWorld
+        return oldWorld.map(replacesCell::replace)
     }
 }
--- src/main/kotlin/me/ohdyno/projects/life/simulate/values/World.kt
+++ src/main/kotlin/me/ohdyno/projects/life/simulate/values/World.kt
@@ -1,7 +1,7 @@
 package me.ohdyno.projects.life.simulate.values
 
-open class World(val width: Int, val height: Int) {
-    protected open val contents: MutableMap<Coordinates, Cell> = mutableMapOf()
+class World(val width: Int, val height: Int) {
+    private val contents: MutableMap<Coordinates, Cell> = mutableMapOf()
 
     fun at(coordinates: Coordinates): Cell {
         return contents[coordinates] ?: Dead()
@@ -10,10 +10,24 @@ open class World(val width: Int, val height: Int) {
     fun with(shape: LifeForm, origin: Coordinates = Coordinates(x = 0, y = 0)): World {
         shape.forEachIndexed { row, columns ->
             columns.forEachIndexed { column, cell ->
-                contents[Coordinates(row, column).translateX(origin.x).translateY(origin.y)] = cell
+                val translatedCoordinates = Coordinates(row, column).translateX(origin.x).translateY(origin.y)
+                contents[translatedCoordinates] = cell
             }
         }
 
         return this
+    }
+
+    fun map(fn: (world: World, coordinates: Coordinates) -> Cell): World {
+        val newWorld = World(this.width, this.height)
+
+        (0 until width).forEach { x ->
+            (0 until height).forEach { y ->
+                val coordinates = Coordinates(x, y)
+                newWorld.contents[coordinates] = fn(this, coordinates)
+            }
+        }
+
+        return newWorld
     }
 }
