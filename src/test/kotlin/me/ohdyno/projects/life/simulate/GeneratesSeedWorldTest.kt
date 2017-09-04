f29cd4a1ef3b7004d9cde1cd423f444456f0f6b0
Xing Zhou <xing@ohdyno.me>
2017-09-04 06:59:34 -0400

    Remove unnecessary custom Assertions and Matchers

--- src/test/kotlin/me/ohdyno/projects/life/simulate/GeneratesSeedWorldTest.kt
+++ src/test/kotlin/me/ohdyno/projects/life/simulate/GeneratesSeedWorldTest.kt
@@ -1,6 +1,9 @@
 package me.ohdyno.projects.life.simulate
 
-import me.ohdyno.projects.life.simulate.matchers.Life
+import com.natpryce.hamkrest.assertion.assert
+import com.natpryce.hamkrest.equalTo
+import me.ohdyno.projects.life.simulate.values.Cell.Dead
+import me.ohdyno.projects.life.simulate.values.Cell.Live
 import me.ohdyno.projects.life.simulate.values.Coordinates
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
@@ -13,18 +16,17 @@ object GeneratesSeedWorldTest : SubjectSpek<GeneratesSeedWorld>({
         val result = subject.generate()
 
         it("generates a world with a glider") {
+            assert.that(result.at(Coordinates(y = 0, x = 0)), equalTo(Dead))
+            assert.that(result.at(Coordinates(y = 0, x = 1)), equalTo(Live))
+            assert.that(result.at(Coordinates(y = 0, x = 2)), equalTo(Dead))
 
-            Life.`in`(result).at(Coordinates(y = 0, x = 0)).isDead()
-            Life.`in`(result).at(Coordinates(y = 0, x = 1)).isAlive()
-            Life.`in`(result).at(Coordinates(y = 0, x = 2)).isDead()
+            assert.that(result.at(Coordinates(y = 1, x = 0)), equalTo(Dead))
+            assert.that(result.at(Coordinates(y = 1, x = 1)), equalTo(Live))
+            assert.that(result.at(Coordinates(y = 1, x = 2)), equalTo(Live))
 
-            Life.`in`(result).at(Coordinates(y = 1, x = 0)).isDead()
-            Life.`in`(result).at(Coordinates(y = 1, x = 1)).isAlive()
-            Life.`in`(result).at(Coordinates(y = 1, x = 2)).isAlive()
-
-            Life.`in`(result).at(Coordinates(y = 2, x = 0)).isAlive()
-            Life.`in`(result).at(Coordinates(y = 2, x = 1)).isDead()
-            Life.`in`(result).at(Coordinates(y = 2, x = 2)).isAlive()
+            assert.that(result.at(Coordinates(y = 2, x = 0)), equalTo(Live))
+            assert.that(result.at(Coordinates(y = 2, x = 1)), equalTo(Dead))
+            assert.that(result.at(Coordinates(y = 2, x = 2)), equalTo(Live))
         }
     }
 })
